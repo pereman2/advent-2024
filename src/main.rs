16cc9cd1986a032faf2fc9383b14eefc26d9fc66
Pere Diaz Bou <pere-altea@hotmail.com>
2024-12-01 10:25:54 +0100

    day 1

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+#![feature(test)]
+extern crate test;
+
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+macro_rules! bench_fns {
+    ($(($name:ident, $func_name:ident)),*) => {
+        $(
+            #[bench]
+            fn $name(b: &mut test::Bencher) {
+                b.iter(|| $func_name());
+            }
+        )*
+    };
+}
+
+struct Parser {
+    file: BufReader<File>,
+}
+
+impl Parser {
+    pub fn new(path: &str) -> Self {
+        Self {
+            file: BufReader::new(File::open(path).unwrap()),
+        }
+    }
+
+    pub fn next_line<'a>(&mut self, buf: &'a mut Vec<u8>) -> (usize, &'a str) {
+        buf.clear();
+        let res = self.file.read_until('\n' as u8, buf).unwrap();
+        let line = unsafe {
+            let len = if res == 0 { 0 } else { buf.len() - 1 };
+            std::str::from_utf8_unchecked(&buf.as_slice()[0..len])
+        };
+        (res, line)
+    }
+}
+
+fn day_1_1() {
+    let mut parser = Parser::new("input1.txt");
+    let mut buf = Vec::with_capacity(4096);
+    let mut vv = Vec::new();
+    let mut ww = Vec::new();
+    loop {
+        let (res, line) = parser.next_line(&mut buf);
+        if res == 0 {
+            break;
+        }
+
+        let mut spaces = line.split(" ");
+        let a = spaces.next().unwrap().parse::<i64>().unwrap();
+        let b = spaces.next().unwrap().parse::<i64>().unwrap();
+        vv.push(a);
+        ww.push(b);
+    }
+
+    vv.sort();
+    ww.sort();
+
+    let mut res = 0;
+    for i in 0..vv.len() {
+        res += vv[i].abs_diff(ww[i]);
+    }
+    dbg!(res);
+}
+
+fn day_1_2() {
+    let mut parser = Parser::new("input1.txt");
+    let mut buf = Vec::with_capacity(4096);
+    let mut vv = HashSet::new();
+    let mut ww = HashMap::new();
+    loop {
+        let (res, line) = parser.next_line(&mut buf);
+        if res == 0 {
+            break;
+        }
+
+        let mut spaces = line.split(" ");
+        let a = spaces.next().unwrap().parse::<i64>().unwrap();
+        let b = spaces.next().unwrap().parse::<i64>().unwrap();
+        vv.insert(a);
+        if !ww.contains_key(&b) {
+            ww.insert(b, 1);
+        } else {
+            let v = ww.get_mut(&b).unwrap();
+            *v += 1;
+        }
+    }
+
+    let mut res = 0;
+    for i in vv {
+        if let Some(v) = ww.get(&i) {
+            res += i * *v;
+        }
+    }
+    dbg!(res);
+}
+
+bench_fns!((bench_day_1_1, day_1_1), (bench_day_1_2, day_1_2));
+
+fn main() {
+    day_1_2();
+}
